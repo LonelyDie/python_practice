1491fe2c7771202c7952e573f4eaecebc3239b26
Pavel Pavlovskiy <pasha18plus@gmail.com>
2025-02-02 19:33:56 +0300

    9.2 Срезы

---
+++ practice/functions_loops.py
@@ -0,0 +1,61 @@
+# Возведение в 3-ю степень
+def cube(x):
+    product = pow(x, 3)
+    return product
+
+
+a = int(input())
+num = cube(a)
+print(num)
+
+
+# Вывод имени
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+b = input()
+greet(b)
+
+
+# Температура по шкале Фаренгейта
+def convert_far_to_cel(F):
+    C = (F - 32) * 5 / 9
+    print(f"{F} degrees F = {C:.2f} degrees F")
+    return F
+
+
+F = int(input("Enter a temperature in degrees F: "))
+convert_far_to_cel(F)
+
+
+# Температура по шкале Цельсия
+def convert_cel_to_far(C):
+    F = C * 9 / 5 + 32
+    print(f"{C} degrees C = {F:.2f} degrees F")
+    return F
+
+
+C = int(input("Enter a temperature in degrees C: "))
+convert_cel_to_far(C)
+
+
+# Целые числа от 2 до 10
+for i in range(2, 11):
+    print(i)
+
+# Целые числа от 2 до 10
+n = 1
+while n < 10:
+    n += 1
+    print(n)
+
+def doubles(num):
+    total = 0
+    for i in range(num):
+        total += num
+    return total
+
+
+num3 = int(input())
+doubles(num3)
