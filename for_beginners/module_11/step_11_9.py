e0054f7eb7c3e234541e2a6fc2b8255eba34a6d8
Paulo <pasha18plus@gmail.com>
2025-02-06 14:04:52 +0300

    11.1 Введение в списки, 11.2 Основы работы со списками

---
+++ for_beginners/module_11/step_11_9.py
@@ -0,0 +1,8 @@
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+print(languages[::-1])
+
+
+
+""" Дополните приведённый ниже код так, чтобы он вывел "перевёрнутый" список languages (то есть элементы будут идти в обратном порядке).
+
+Примечание. Для вывода списка воспользуйтесь функцией print(). """
